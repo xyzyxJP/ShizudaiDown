b3de8e8c68a85140fb2e66fcb36a70c2fd11f0d0
Yuto Sahashi <sahashi2002@gmail.com>
2022-03-29 11:35:20 +0900

    Fix

--- detector.py
+++ detector.py
@@ -1,16 +1,26 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from lxml import html
 import os
-import psycopg2
+import psycopg2 as pg
 import requests
 import time
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import datetime
+import seaborn as sns
+import tweepy
 
 session = requests.session()
-USERNAME = os.environ.get('GAKUJO_USERNAME')
-PASSWORD = os.environ.get('GAKUJO_PASSWORD')
 apache = None
 
+GAKUJO_USERNAME = os.environ.get('GAKUJO_USERNAME')
+GAKUJO_PASSWORD = os.environ.get('GAKUJO_PASSWORD')
 DATABASE_URL = os.environ.get('DATABASE_URL')
+TWITTER_ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+TWITTER_ACCESS_SECRET = os.environ.get('TWITTER_ACCESS_SECRET')
+TWITTER_API_KEY = os.environ.get('TWITTER_API_KEY')
+TWITTER_API_SECRET = os.environ.get('TWITTER_API_SECRET')
 
 
 def calc_duration():
@@ -26,10 +36,10 @@ def calc_duration():
     session.get(
         'https://idp.shizuoka.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s1')
     login = session.post('https://idp.shizuoka.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s1', data={
-        'j_username': USERNAME, 'j_password': PASSWORD, '_eventId_proceed': ''})
+        'j_username': GAKUJO_USERNAME, 'j_password': GAKUJO_PASSWORD, '_eventId_proceed': ''})
     if ('ユーザ名またはパスワードが正しくありません。' in login.text):
         return 0
-    bs = BeautifulSoup(login.text, 'html.parser')
+    bs = bs(login.text, 'html.parser')
     relay_state = html.fromstring(str(bs)).xpath(
         '/html/body/form/div/input[1]/@value')[0]
     saml_response = html.fromstring(str(bs)).xpath(
@@ -40,7 +50,7 @@ def calc_duration():
                 headers={'Referer': 'https://idp.shizuoka.ac.jp/'})
     login = session.post('https://gakujo.shizuoka.ac.jp/portal/home/home/initialize',
                          data={'EXCLUDE_SET': ''}, headers={'Referer': 'https://idp.shizuoka.ac.jp/'})
-    bs = BeautifulSoup(login.text, 'html.parser')
+    bs = bs(login.text, 'html.parser')
     apache = html.fromstring(str(bs)).xpath(
         '/html/body/div[1]/form[1]/div/input/@value')
     end_time = time.perf_counter()
@@ -48,18 +58,45 @@ def calc_duration():
 
 
 def add_duration(duration):
-    with psycopg2.connect(DATABASE_URL) as connect:
+    with pg.connect(DATABASE_URL) as connect:
         with connect.cursor() as cursor:
             cursor.execute(
-                "INSERT INTO login_logs(duration) VALUES (%s)", (duration,))
+                'INSERT INTO login_logs(duration) VALUES (%s)', (duration,))
 
 
 def get_recent_durations():
-    with psycopg2.connect(DATABASE_URL) as connect:
-        with connect.cursor(psycopg2.extras.DictCursor) as cursor:
+    with pg.connect(DATABASE_URL) as connect:
+        with connect.cursor() as cursor:
             cursor.execute(
-                "SELECT * FROM login_logs ORDER BY timestamp limit 10")
-            return cursor.fetchall()
+                'SELECT * FROM login_logs ORDER BY timestamp limit 48')
+            return pd.dataframe(cursor.fetchall())
+
+
+def plot_durations():
+    df = get_recent_durations()
+    # df = pd.DataFrame(
+    # data={'timestamp': [datetime.datetime(2022, 3, 29, 0, 0, 0), datetime.datetime(2022, 3, 29, 1, 0, 0), datetime.datetime(2022, 3, 29, 2, 0, 0)], 'duration': [11, 9, 12]})
+    fig, ax = plt.subplots()
+    ax.plot(df['timestamp'], df['duration'], color='#fe923a', linewidth=3)
+    fig.set_facecolor('#212529')
+    ax.set_facecolor('#343a40')
+    ax.tick_params(axis='x', colors='#f8f9fa', size=12)
+    ax.tick_params(axis='y', colors='#f8f9fa', size=12)
+    ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
+    plt.ylim(5, 20)
+    ax.axhspan(ymin=15, ymax=99, color='#c92a2a', alpha=0.3)
+    ax.text(0.99, 0.02, '@ShizudaiDown', horizontalalignment='right',
+            transform=ax.transAxes, color='#f8f9fa', size=12)
+    fig.savefig('plot.png')
+
+
+def post_tweet():
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+    t = tweepy.API(auth)
+    t.update_with_media(status='Hello World from Heroku.', filename='plot.png')
 
 
 add_duration(calc_duration())
+plot_durations()
+post_tweet()
